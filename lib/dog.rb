8ad4b9c98c5f6661496f8581f181b19cdcac4c0a
Ismail Jalloh <ijalloh21@gmail.com>
2018-11-08 04:44:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
 class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
   def name
     @name = name
   end
   
-  def name=(dog_name)
-    @name = dog_name
-  end
+  
   
   def breed
     @breed = 
